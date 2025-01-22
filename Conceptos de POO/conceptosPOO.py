46278f9607e454cf220362cc8722c5761a1a415a
lovelace <alderat0n@protonmail.com>
2025-01-21 20:15:46 -0500

    Actualización de código

--- Conceptos de POO/conceptosPOO.py
+++ Conceptos de POO/conceptosPOO.py
@@ -6,27 +6,29 @@ class Biblioteca:
     def __init__(self):
         self.libros_disponibles = []
         self.libros_reservador = []
-        self.transacciones = []
+        self.reservas = []
 
     def agregar_libro(self, libro):
         self.libros_disponibles.append(libro)
 
-    def reservar_libro(self, libro):
-        if libro in self.libros_disponibles:
-            self.libros_reservador.append(libro)
-            self.libros_disponibles.remove(libro)
-            self.transacciones.append(f"Libro {libro} reservado")
-        else:
-            self.transacciones.append(f"Libro {libro} no disponible")
+    def reservar_libro(self, titulo, tipo):
+        for libro in self.libros_disponibles:
+            if libro.titulo == titulo and libro.tipo == tipo:
+                self.libros_reservador.append(libro)
+                self.libros_disponibles.remove(libro)
+                self.reservas.append(f"Libro '{titulo}' ({tipo}) reservado")
+                return
+        self.reservas.append(f"Libro '{titulo}' ({tipo}) no disponible")
 
-    def trasnsacciones (self):
-        if not self.transacciones:
-            print("No hay transacciones")
+    def historial_reservas (self):
+        if not self.libros_reservador:
+            print("No hay reservas")
         else:
-            for transacciones in self.transacciones:
-                print(f"- {transacciones}")
+            print("\nReservas:")
+            for libro in self.libros_reservador:
+                print(f"- {libro.descripcion()}")
 
-class libro:
+class Libro:
     def __init__(self, genero, titulo, autor, editorial, fecha_lanzamiento, tipo):
         self.genero = genero
         self.titulo = titulo
@@ -41,19 +43,19 @@ class libro:
 if __name__ == "__main__":
     biblioteca = Biblioteca()
 
-    biblioteca.agregar_libro(libro("Novela","1984", "George Orwell", "Editorial El Libro", "1949", "Libro"))
-    biblioteca.agregar_libro(libro("Novela", "1984", "George Orwell", "Editorial El Libro", "1949", "Libro Digital"))
-    biblioteca.agregar_libro(libro("Ciencia Ficcón", "El Juego de Ender", "Orson Scott Card", "Editorial Ficción", "1985", "Libro Digital"))
-    biblioteca.agregar_libro(libro("Novela/Misrerio/Aventura", "La Sombra del Viento", "Carlos Ruiz Safón", "Editorial mágica", "2001", "Libro"))
-    biblioteca.agregar_libro(libro("Novela/Misterio/Aventura", "La Sombra del Viento", "Carlos Ruiz Safón", "Editorial mágica", "2001", "Libro Digital"))
-    biblioteca.agregar_libro(libro("Novela clásica/Aventura", "Moby Dick", "Herman Melville", "Editorial El Libro", "1851", "Libro"))
-    biblioteca.agregar_libro(libro("Novela Histórica/Misterio", "El nombre de la Rosa", "Umberto Eco", "Editorial mágica", "1980", "Libro"))
+    biblioteca.agregar_libro(Libro("Novela","1984", "George Orwell", "Editorial El Libro", "1949", "Libro"))
+    biblioteca.agregar_libro(Libro("Novela", "1984", "George Orwell", "Editorial El Libro", "1949", "Libro Digital"))
+    biblioteca.agregar_libro(Libro("Ciencia Ficcón", "El Juego de Ender", "Orson Scott Card", "Editorial Ficción", "1985", "Libro Digital"))
+    biblioteca.agregar_libro(Libro("Novela/Misrerio/Aventura", "La Sombra del Viento", "Carlos Ruiz Safón", "Editorial mágica", "2001", "Libro"))
+    biblioteca.agregar_libro(Libro("Novela/Misterio/Aventura", "La Sombra del Viento", "Carlos Ruiz Safón", "Editorial mágica", "2001", "Libro Digital"))
+    biblioteca.agregar_libro(Libro("Novela clásica/Aventura", "Moby Dick", "Herman Melville", "Editorial El Libro", "1851", "Libro"))
+    biblioteca.agregar_libro(Libro("Novela Histórica/Misterio", "El nombre de la Rosa", "Umberto Eco", "Editorial mágica", "1980", "Libro"))
 
     while True:
         print("\n -- Tienda Virtual -- ")
         print("1. Constultar Libro")
         print("2. Reservar libro")
-        print("3. Ver historial de transacciones")
+        print("3. Ver historial de reservas")
         print("4. Salir")
 
         opciones = input("Que deseas hacer? ")
@@ -62,14 +64,18 @@ if __name__ == "__main__":
             print("\nLibros Disponibles")
             for libro in biblioteca.libros_disponibles:
                 print(f"- {libro.descripcion()}")
+
         elif opciones == "2":
-            libro_reservado = input("Ingresa el titulo del libro que desea reservar: ")
-            print(f"Reservando {libro_reservado}")
+            titulo = input("Ingresa el titulo del libro que desea reservar: ")
+            tipo = input("Ingresa el tipo del libro (Libro/Libro Digital): ")
+            biblioteca.reservar_libro(titulo, tipo)
 
         elif opciones == "3":
-            biblioteca.trasnsacciones()
+            biblioteca.historial_reservas()
+
 
         elif opciones == "4":
             print("Saliendo del programa")
+            break
 
 
