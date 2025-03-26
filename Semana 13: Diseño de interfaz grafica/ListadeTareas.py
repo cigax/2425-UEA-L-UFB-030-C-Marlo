f9f78ec535c19674376997d5d24c9206e91f381b
lovelace <alderat0n@protonmail.com>
2025-03-26 00:00:45 -0500

    Diseño de interfaz grafica

--- Semana 13: Diseño de interfaz grafica/ListadeTareas.py
+++ Semana 13: Diseño de interfaz grafica/ListadeTareas.py
@@ -28,14 +28,11 @@ tk.Label(ventana, text="Agrega una tarea:").pack(pady=5)
 entrada = tk.Entry(ventana, width=40)
 entrada.pack(pady=5)
 
-# Botones
 tk.Button(ventana, text="Agregar", command=agregar_tarea).pack(pady=5)
 tk.Button(ventana, text="Eliminar", command=eliminar_tarea).pack(pady=5)
 tk.Button(ventana, text="Borrar todo", command=limpiar_lista).pack(pady=5)
 
-# Lista de tareas
 lista_tareas = tk.Listbox(ventana, width=50, height=15)
 lista_tareas.pack(pady=10)
 
-# Ejecutar la aplicación
 ventana.mainloop()
